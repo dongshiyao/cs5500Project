55539a9e574e14a1cd56f90142c622947f6d3bd1
sdong <dong.shi@husky.neu.edu>
2017-12-06 22:40:09 -0800

    add in default constructor for AnimalRecord

--- src/main/java/css/neu/edu/cs5500/neufoodtruck/model/AnimalRecord.java
+++ src/main/java/css/neu/edu/cs5500/neufoodtruck/model/AnimalRecord.java
@@ -58,7 +58,12 @@ public class AnimalRecord {
     @Column
     private int shelterId;
 
+    public AnimalRecord() {
+
+    }
+
     public AnimalRecord(String category, String breed, int weight, String gender, String color, String status, Timestamp foundTime, Timestamp updateTime, double foundLocationLong, double foundLocationLat, double currentLocationLong, double currentLocationLat, int shelterId) {
+        super();
         this.category = category;
         this.breed = breed;
         this.weight = weight;
